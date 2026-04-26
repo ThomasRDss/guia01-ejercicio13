884b209a5281d7525a335fae98e66729d4b55645
Thomas Rodas <thomas.n.2004.tr@gmail.com>
2026-04-26 11:03:26 -0300

    ejercicio finalizado

--- src/guia1/ejercicio13/PosNet.java
+++ src/guia1/ejercicio13/PosNet.java
@@ -1,15 +1,16 @@
 package guia1.ejercicio13;
 
 public class PosNet {
+
     public static double RECARGO_POR_CUOTAS = 0.03;
     public static int MINIMA_CANTIDAD_CUOTAS = 1;
     public static int MAXIMA_CANTIDAD_CUOTAS = 6;
-    
+
     public Ticket efectuarPago(TarjetaDeCredito tarjetaDeCredito, double montoAPagar, int cantidadDeCuotas) {
         Ticket pagoFinalizado = null;
-        if(tarjetaDeCredito != null && montoAPagar > 0 && cantidadDeCuotas > 0 || cantidadDeCuotas < 7){
+        if (tarjetaDeCredito != null && montoAPagar > 0 && cantidadDeCuotas > 0 || cantidadDeCuotas < 7) {
             montoAPagar += calculoXCuotas(cantidadDeCuotas, montoAPagar);
-            if(tarjetaDeCredito.verificarSaldoTarjeta(tarjetaDeCredito, montoAPagar)){
+            if (tarjetaDeCredito.verificarSaldoTarjeta(tarjetaDeCredito, montoAPagar)) {
                 tarjetaDeCredito.descontar(montoAPagar);
                 String nombreApellido = tarjetaDeCredito.nombreCompleto();
                 double montoPorCuota = montoAPagar / cantidadDeCuotas;
@@ -19,11 +20,12 @@ public class PosNet {
         return pagoFinalizado;
     }
 
-    private double calculoXCuotas(int cantidadDeCuotas, double montoAPagar){
-    if (cantidadDeCuotas == 1) return 0;
-    // Solo aplica recargo a las cuotas que superen la primera
-    double recargoTotal = (cantidadDeCuotas - 1) * RECARGO_POR_CUOTAS; 
-    return recargoTotal * montoAPagar;
+    private double calculoXCuotas(int cantidadDeCuotas, double montoAPagar) {
+        double total = 0;
+        if (cantidadDeCuotas > 1) {
+            total = ((cantidadDeCuotas - 1) * RECARGO_POR_CUOTAS) * montoAPagar;
+        }
+        return total;
     }
 
     @Override
@@ -31,5 +33,4 @@ public class PosNet {
         return "PosNet{" + '}';
     }
 
- 
 }
